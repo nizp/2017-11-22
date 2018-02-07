7d104d91b0b1fb8285072e1abf2d2c0216f8e824
nizp <nzp890524@163.com>
2018-02-07 16:40:55 +0800

    2018-2-7

--- 2018-2-7/my-app/src/todolist/component/list.js
+++ 2018-2-7/my-app/src/todolist/component/list.js
@@ -5,14 +5,20 @@ class List extends Component{
         通过这样的写法，把父级的数据变成了子级的
         两个数据（子数据与父数据）并不干扰。
 
-        子级的constructor中的props只会走一次
+        子级的constructor中的props只会走一次,
+        会接受父级传过来的第一次数据，就算父级数据更新
+        也不会再次更新props了
+
+
+        ctxt主要就是为了存父级的数据，为了如果内容为空的时候
+        value值仍然是初始的那个
     */
     constructor(props){
         super(props);
         this.state = {
             //是否双击
             ondb:false,
-            ctxt:this.props.txt
+            ctxt:''
         }
     }
     checked = () => {
@@ -24,8 +30,10 @@ class List extends Component{
         rm(id);
     }
     db = () => {
+        //双击的时候把父级的第一次传来的数据存起来
         this.setState({
-            ondb : true
+            ondb : true,
+            ctxt:this.props.txt
         },()=>{
             /*
                 ()=>{}为数据更新之后的回调函数。
@@ -43,22 +51,17 @@ class List extends Component{
     }
 
     blur = () => {
-        let {ctxt} = this.state;
         let {txt,changeVal,id} = this.props;
-        if(ctxt){
-            //通知父级修改子级中的值
-            changeVal(id,ctxt);
-            this.setState({ondb:false});
-        }else{
-            //没内容
-            this.setState({ondb:false,ctxt:txt});
+        //如果txt为空的，那么内容得还原
+        if(!txt){
+            changeVal(id,this.state.ctxt);
         }
+        this.setState({ondb:false});
     }
 
     change = (ev) => {
-        this.setState({
-            ctxt:ev.target.value
-        })
+       let {changeVal,id} = this.props;
+       changeVal(id,this.refs.nn.value);
     }
 
     down = (ev) => {
@@ -69,11 +72,12 @@ class List extends Component{
 
     render(){
         let {txt,checked} = this.props;
-        let {ondb,ctxt} = this.state;
+        let {ondb} = this.state;
         let classN = checked?'completed':'';
         if(ondb){
             classN += ' editing'; //"completed editing"
         }
+        console.log(txt)
         return (
             <li 
                 className={classN} 
@@ -94,7 +98,7 @@ class List extends Component{
                 </div>
                 <input 
                     className="edit" 
-                    value={ctxt} 
+                    value={txt} 
                     onBlur={this.blur}
                     onChange = {this.change}
                     ref="nn"
